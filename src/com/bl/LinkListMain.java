a281b5fbcb2ba2716bce5c73eea15381e46bf369
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-08 20:08:50 +0530

    Added : welcome message

---
+++ src/com/bl/LinkListMain.java
@@ -0,0 +1,19 @@
+package com.bl;
+
+import java.util.LinkedList;
+
+public class LinkListMain {
+
+    public static void main(String[] args) {
+
+        LinkedList <Integer> linklist1 = new LinkedList();
+        linklist1.add(56);
+        linklist1.add(30);
+        linklist1.add(70);
+
+        System.out.println("LinkList =>" + linklist1);
+
+
+
+    }
+}
