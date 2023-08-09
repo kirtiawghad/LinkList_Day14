23089a786eb84e780440e1e202f03ca6fd1529a8
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-09 12:57:13 +0530

    Added : UC2

---
+++ src/com/bl/LinkedListMain.java
@@ -0,0 +1,23 @@
+package com.bl;
+
+import java.util.LinkedList;
+
+public class LinkedListMain {
+
+
+  
+       
+    public static void main(String[] args) {
+
+        LinkedList <Integer> linkedList1 = new LinkedList<>();
+
+        linkedList1.add(70);
+        linkedList1.add(30);
+        linkedList1.add(56);
+
+       linkedList1.display();
+
+
+
+    }
+}
