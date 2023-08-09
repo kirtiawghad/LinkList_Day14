981a8eff2e9382880a830a685c5bb11d9be56924
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-09 13:04:49 +0530

    Added : UC3

--- src/com/bl/LinkedList.java
+++ src/com/bl/LinkedList.java
@@ -6,27 +6,41 @@ public class LinkedList <E> {
 
     Node<E> tail ;
 
-    public void add(E data) {
+    public void add(E data){
         Node<E> newNode = new Node<>(data);
-        if (head == null) {
+        if(head == null){
             head = newNode;
             tail = newNode;
-        } else {
+        }
+        else {
             newNode.setNext(head);
             head = newNode;
         }
     }
-        public void display() {
-            Node<E> temp = head;
-            if(head == null){
-                System.out.println("Linked List is empty");
-            }
-            while(temp != null){
-                System.out.print(temp.getData() + " ");
-                temp = temp.getNext();
-            }
+
+    public void display() {
+        Node<E> temp = head;
+        if(head == null){
+            System.out.println("Linked List is empty");
+        }
+        while(temp != null){
+            System.out.print(temp.getData() + " ");
+            temp = temp.getNext();
         }
     }
 
+    public void append(E data) {
+        Node<E> newNode = new Node<>(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+
+}
 
 
--- src/com/bl/LinkedListMain.java
+++ src/com/bl/LinkedListMain.java
@@ -1,23 +1,23 @@
 package com.bl;
-
-import java.util.LinkedList;
-
 public class LinkedListMain {
-
-
-  
-       
     public static void main(String[] args) {
 
-        LinkedList <Integer> linkedList1 = new LinkedList<>();
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
 
         linkedList1.add(70);
         linkedList1.add(30);
         linkedList1.add(56);
 
-       linkedList1.display();
+        linkedList1.display();
+        System.out.println();
 
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.append(56);
+        linkedList2.append(30);
+        linkedList2.append(70);
 
+        linkedList2.display();
 
     }
+
 }
